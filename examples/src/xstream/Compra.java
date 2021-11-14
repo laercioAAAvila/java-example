a8defcd287110df8ef7f53469458a69c18cf6ac7
Laercio Alberto A. de Ávila <92470064+laercioAAAvila@users.noreply.github.com>
2021-11-14 16:34:55 -0300

    Create Compra and CompraTeste class

--- examples/src/xstream/Compra.java
+++ examples/src/xstream/Compra.java
@@ -1,4 +1,14 @@
 package xstream;
 
+import java.util.*;
+
 public class Compra {
+
+    private int id;
+    private List<Produto> produtoList = new ArrayList<>();
+
+    public Compra(int id, List<Produto> produtoList) {
+        this.id = id;
+        this.produtoList = produtoList;
+    }
 }
---
+++ examples/src/xstream_test/CompraTeste.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class CompraTeste {
+
+    @Test
+    public void deveGerarCadaUmDosProdutosDeUmaCompra(){
+
+    }
+}
